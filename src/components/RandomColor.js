a41b5148d6f2135a8bc70a2c948f59461691c1b5
Sean Nelson <smnelsonn@gmail.com>
2019-05-14 17:09:17 -0700

    working well

--- src/components/RandomColor.js
+++ src/components/RandomColor.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import style from './RandomColor.css';
 
 export default class RandomColor extends PureComponent {
   state = {
@@ -13,21 +14,226 @@ export default class RandomColor extends PureComponent {
     return colors[randomNumber];
   }
 
-
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.color === this.state.color){
+      this.setState({ img: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjG7oqSl5ziAhVlCTQIHVlVAR4QjRx6BAgBEAU&url=https%3A%2F%2Fwww.ebaumsworld.com%2Fpictures%2F19-random-pictures-that-will-get-you-through-the-next-few-seconds%2F85708056%2F&psig=AOvVaw07imtoPEXWmVK_Qz905u7a&ust=1557963148662334' });
+    }
+  }
   componentDidMount() {
     this.intervalId = setInterval(() => {
       this.setState({ color: this.randomize(), img: '' });
-    }, 500);
+    }, 100);
   }
 
+
   render() { 
-    const color = this.state.color;
+    const color = this.randomize();
+    const { img } = this.state;
+    const colorAlt = this.randomize();
+
     const divStyle = {
-      backgroundColor: color,
-      height: '50000px',
-      width: '10000px'
+      background: img ? `center / contain url(${img})` : color,
+      height: '100px',
+      width: '200px',
+      margin: '10px'
+    };
+
+    const divStyleAlt = {
+      background: img ? `center / contain url(${img})` : colorAlt,
+      height: '100px',
+      width: '200px',
+      margin: '10px'
     };
     
-    return <div style={divStyle}></div>;
+    return (
+      <section className={style.major}>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+        <div style={divStyle}></div>
+        <div style={divStyleAlt}></div>
+      </section>
+    );
   }
 }
