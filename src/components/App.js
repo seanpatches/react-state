1492752958f787b9881673a53fb69a87c0755c89
Sean Nelson <smnelsonn@gmail.com>
2019-05-15 09:49:02 -0700

    random intervals, one div in component, removed dog pic

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,53 @@
 import React from 'react';
 import RandomColor from './RandomColor.js';
+import style from './RandomColor.css';
+
 
 export default function App() {
-  return <RandomColor />;
+  return (
+    <section className={style.major}>
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+      <RandomColor />
+    </section>
+  );
 }
--- src/components/RandomColor.js
+++ src/components/RandomColor.js
@@ -18,7 +18,7 @@ export default class RandomColor extends PureComponent {
   componentDidMount() {
     this.intervalId = setInterval(() => {
       this.setState({ color: this.randomize(), img: '', trigger: new Date() });
-    }, 1000);
+    }, (Math.random() * 1000));
   }
 
   componentDidUpdate(prevProps, prevState){
@@ -31,15 +31,6 @@ export default class RandomColor extends PureComponent {
   render() { 
     const color = this.state.color;
     const { img } = this.state;
-    const colorAlt = this.randomize();
-    const color2 = this.randomize();
-    const color3 = this.randomize();
-    const color4 = this.randomize();
-    const color5 = this.randomize();
-    const color6 = this.randomize();
-    const color7 = this.randomize();
-    const color8 = this.randomize();
-    const color9 = this.randomize();
 
     const divStyle = {
       background: img ? `center / contain url(${img})` : color,
@@ -47,221 +38,9 @@ export default class RandomColor extends PureComponent {
       width: '200px',
       margin: '10px'
     };
-
-    const divStyleAlt = {
-      background: img ? `center / contain no-repeat url(${img})` : colorAlt,
-      height: '100px',
-      width: '200px',
-      margin: '10px'
-    };
-    const divStyle2 = {
-      background: img ? `center / contain url(${img})` : color2,
-      height: '100px',
-      width: '200px',
-      margin: '10px'
-    };
-    const divStyle3 = {
-      background: img ? `center / contain url(${img})` : color3,
-      height: '100px',
-      width: '200px',
-      margin: '10px'
-    };
-    const divStyle4 = {
-      background: img ? `center / contain url(${img})` : color4,
-      height: '100px',
-      width: '200px',
-      margin: '10px'
-    };
-    const divStyle5 = {
-      background: img ? `center / contain url(${img})` : color5,
-      height: '100px',
-      width: '200px',
-      margin: '10px'
-    };
-    const divStyle6 = {
-      background: img ? `center / contain url(${img})` : color6,
-      height: '100px',
-      width: '200px',
-      margin: '10px'
-    };
-    const divStyle7 = {
-      background: img ? `center / contain url(${img})` : color7,
-      height: '100px',
-      width: '200px',
-      margin: '10px'
-    };
-    const divStyle8 = {
-      background: img ? `center / contain url(${img})` : color8,
-      height: '100px',
-      width: '200px',
-      margin: '10px'
-    };
-    const divStyle9 = {
-      background: img ? `center / contain url(${img})` : color9,
-      height: '100px',
-      width: '200px',
-      margin: '10px'
-    };
     
     return (
-      <section className={style.major}>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-        <div style={divStyle}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle2}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle4}></div>
-        <div style={divStyleAlt}></div>
-        <div style={divStyle5}></div>
-        <div style={divStyle6}></div>
-        <div style={divStyle3}></div>
-        <div style={divStyle7}></div>
-        <div style={divStyle8}></div>
-        <div style={divStyle9}></div>
-      </section>
+      <div style={divStyle}></div>
     );
   }
 }
